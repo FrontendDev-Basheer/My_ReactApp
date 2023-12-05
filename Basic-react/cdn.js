260a07a674ab79b390fb0202b965566cec1f47ae
FrontendDev-Basheer <basheermmmofficial@gmail.com>
2023-12-05 20:02:46 +0530

    my-react-app

---
+++ Basic-react/cdn.js
@@ -0,0 +1,3 @@
+const element = React.createElement("h1",{id:"myheading"},"Welcome to Website");
+const getElement = ReactDOM.createRoot(document.getElementById("root"));
+getElement.render(element);
