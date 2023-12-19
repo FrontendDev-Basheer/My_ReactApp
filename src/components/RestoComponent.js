fd972c3653bf50fc301e39cbe778a4d0eccc884a
FrontendDev-Basheer <basheermmmofficial@gmail.com>
2023-12-19 08:55:09 +0530

    use effect

--- src/components/RestoComponent.js
+++ src/components/RestoComponent.js
@@ -6,7 +6,7 @@ const RestsurantComp = (props) => {
         <div className="scroll-menu">
             <div className="card">
             <div className="card-body">              
-                <img src="https://tse3.mm.bing.net/th?id=OIP.n5kTYovUzLXXmy_FH0XVpgHaE3&pid=Api&P=0&h=180" alt="food"></img>
+                <img className="" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + props.resData.info.cloudinaryImageId} alt="food"></img>
                 <h5 className="card-title">{props.resData.info.name}</h5>
                 <p className="card-text">Rating : {props.resData.info.avgRatingString}</p>
                 <p className="card-text">{props.resData.info.locality}</p>
