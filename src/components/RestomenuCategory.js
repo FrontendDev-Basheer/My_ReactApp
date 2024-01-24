cd112f3d3846914180bf577c1f58e6c42be8a018
FrontendDev-Basheer <basheerboss788@gmail.com>
2024-01-24 14:00:20 +0530

    category

--- src/components/RestomenuCategory.js
+++ src/components/RestomenuCategory.js
@@ -1,9 +1,9 @@
 
-const RestomrenuCategory = () => {
-    
+const RestomrenuCategory = ({data}) => {
+ console.log(data)
     return(
         <div className="category-head">
-           <h1 className="caregory-title">Hello</h1>
+           <h1 className="caregory-title">{data.title} - ({data.itemCards.length})</h1>
         </div>
      )
 
--- src/components/RestomenuComponent.js
+++ src/components/RestomenuComponent.js
@@ -10,31 +10,36 @@ const Restomenu = () => {
 
     useEffect(()=>{
         fetchMenu();
-    });
+    },[]);
 
     const fetchMenu = async() => {
         const RestomenuData = await axios.get ("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.6287557&lng=79.4191795&restaurantId="+resId);
         // const menuDatajson = await RestomenuData.json();
         SetrestomenuInfo(RestomenuData.data);
-        //console.log(RestomenuData.data);
-        const itemcardslist = RestomenuData.data.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards;
+        console.log(RestomenuData.data);
+       // const itemcardslist = RestomenuData.data.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards;
        // console.log(itemcardslist);
        
 
     } 
     
-    const categories = restomenuInfo?.data?.cards[2]?.groupedCard?.cardGroupMap.REGULAR.cards.filter(
+    const categories = restomenuInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR.cards.filter(
         (cd) => cd.card?.card?.["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
-    console.log(categories);
-    //const {name,locality} = restomenuInfo?.data?.cards[0]?.card?.card?.info;
+    //console.log(categories);
+    
+    // Destrcuring
+    // const {name} = restomenuInfo?.data?.cards[0]?.card?.card?.info;
+    // console.log(name);
     return (
         <div className="restomenubar">
-            {/* {
-                categories.map((category) => (
-                    <RestomrenuCategory/>
+            
+           
+            {
+                categories?.map((category) => (
+                    <RestomrenuCategory data = {category?.card?.card}/>
                 ))
-            } */}
-       <RestomrenuCategory/>
+            }
+     
             
         </div>
         
