f701eee522d1995adaa3d94d5c0adb551853f315
FrontendDev-Basheer <basheermmmofficial@gmail.com>
2024-01-25 07:54:25 +0530

    newcomp

--- src/components/RestomenuCategory.js
+++ src/components/RestomenuCategory.js
@@ -1,20 +1,11 @@
 
-<<<<<<< HEAD
-
-const RestomenuCategory = ({catdata}) => {
-    console.log(catdata);
-    return(
-        <div className="category-head">
-           <h1 className="caregory-title">{catdata.title}</h1>
-=======
-const RestomrenuCategory = () => {
-    
-    return(
-        <div className="category-head">
-           <h1 className="caregory-title">Hello</h1>
->>>>>>> 11975005fb71ca51b8ab6d3bfe6a3bb4c0e9800a
-        </div>
-     )
-
-};
-export default RestomenuCategory;
+const RestomenuCategory = ({data}) => {
+    console.log(data)
+       return(
+           <div className="category-head">
+              <h1 className="caregory-title">{data.title}</h1>
+           </div>
+        )
+   
+   };
+   export default RestomenuCategory;
